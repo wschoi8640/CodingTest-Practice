d8cd1ecdf8c2c267d3f2c6130a9fe5c5d788bafd
wschoi8640 <cws911112@gmail.com>
2019-06-07 17:57:15 +0900

    UPDATE Modifications(ADD Remarks to files)

--- src/com/wschoi/practice/bruteforce/WhoPicksBestAnswer.java
+++ src/com/wschoi/practice/bruteforce/WhoPicksBestAnswer.java
@@ -4,8 +4,25 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+/**
+ * 수포자는 수학을 포기한 사람의 준말입니다. 수포자 삼인방은 모의고사에 수학 문제를 전부 찍으려 합니다. 수포자는 1번 문제부터 마지막 문제까지 다음과 같이 찍습니다.
+ * 
+ * 1번 수포자가 찍는 방식: 1, 2, 3, 4, 5, 1, 2, 3, 4, 5, ...
+ * 2번 수포자가 찍는 방식: 2, 1, 2, 3, 2, 4, 2, 5, 2, 1, 2, 3, 2, 4, 2, 5, ...
+ * 3번 수포자가 찍는 방식: 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, 3, 3, 1, 1, 2, 2, 4, 4, 5, 5, ...
+ * 1번 문제부터 마지막 문제까지의 정답이 순서대로 들은 배열 answers가 주어졌을 때, 가장 많은 문제를 맞힌 사람이 누구인지 배열에 담아 return 하도록 solution 함수를 작성해주세요.
+
+ * @author cws86
+ */
+
 public class WhoPicksBestAnswer
 {
+	/**
+	 * 배열 크기에 맞게 % 연산자를 잘 이용해서 반복문 처리
+	 * 
+	 * @param answers
+	 * @return
+	 */
 	public static int[] solution(int[] answers)
 	{
 		int[] student1 = {1, 2, 3, 4, 5};
